dad33fdfd03b55abcc1ceedb4e5e9073911087f1
rfakhrifadlan <rfakhrifadlan@gmail.com>
2022-06-09 22:32:04 +0700

    Done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,35 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let res = [];
+
+  fs.readFile(file1, "utf8", (err, data) => {
+    if (err) {
+      return err;
+    }
+    let mentah1 = JSON.parse(data);
+    res.push(mentah1.message.split(" ")[1]);
+
+    fs.readFile(file2, "utf8", (err, data2) => {
+      if (err) {
+        return err;
+      }
+      let mentah2 = JSON.parse(data2);
+      res.push(mentah2[0].message.split(" ")[1]);
+
+      fs.readFile(file3, "utf8", (err, data3) => {
+        if (err) {
+          return err;
+        }
+        let mentah3 = JSON.parse(data3);
+        res.push(mentah3[0]["data"].message.split(" ")[1]);
+
+        return fnCallback(null, res);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
